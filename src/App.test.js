4291090c2d9cda56b9d65af322bd3ccd082d615d
Deke <Dekefective@gmail.com>
2022-01-26 13:00:41 -0500

    Updated App.js tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Now fails since I removed 'learn react' text
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
+// Look for new text in App
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Github Actions");
   expect(linkElement).toBeInTheDocument();
 });
